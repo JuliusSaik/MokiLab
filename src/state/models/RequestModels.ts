9afdde1845a650c343b9753ce29fd51e42ba1fa8
juliussaikauskas <juliusaikauskas@gmail.com>
2025-05-12 18:30:02 +0300

    adding validation, fixing styling

--- src/state/models/RequestModels.ts
+++ src/state/models/RequestModels.ts
@@ -1,3 +1,10 @@
+export type Fields =
+  | "subject"
+  | "grade"
+  | "topic"
+  | "difficulty"
+  | "detailedTopicPrompt";
+
 export interface RequestPrompt {
   subject: string;
   grade: number;
@@ -6,4 +13,19 @@ export interface RequestPrompt {
   detailedTopicPrompt: string;
 }
 
+// 0 is subject and so on...
+export const StepToFieldMapping: Fields[] = [
+  "subject",
+  "grade",
+  "topic",
+  "difficulty",
+  "detailedTopicPrompt",
+];
+
 export type DifficultiesTypes = "lengvi" | "vidutiniai" | "sunkūs" | "maišyti";
+export const Difficulties: DifficultiesTypes[] = [
+  "lengvi",
+  "vidutiniai",
+  "sunkūs",
+  "maišyti",
+];
